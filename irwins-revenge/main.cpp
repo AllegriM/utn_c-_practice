2da66db400babcbec3eca1b146619fb11669fedf
AllegriM <elmonky99@gmail.com>
2023-06-08 00:33:56 -0300

    feat: :white_check_mark: add functions

--- irwins-revenge/main.cpp
+++ irwins-revenge/main.cpp
@@ -50,98 +50,208 @@ void lanzarCincoDados(int dados[]) {
 }
 
 void startGame() {
-  const int NUM_ESTATUILLAS = 5;
-  int opcion, dadosFaseFinal[5] = {}, dadosExpedicion[2] = {};
-  string estatuillasSteve[5] = {};
+  int NUM_ESTATUILLAS = 5, contadorEstatuillasObtenidas = 0;
+  string jugador1, jugador2;
+  string jugador1Estatuillas[5] = {}, jugador2Estatuillas[5] = {};
+  int opcion, dadosFaseFinal[5] = {}, dadosExpedicion[2] = {},
+              dadoOrdenJugadores[2] = {};
+  bool estatuillasDisponibles[NUM_ESTATUILLAS] = {true, true, true, true, true};
   string estatuillas[NUM_ESTATUILLAS] = {"Hormiga", "Cangrejo", "Medusa",
                                          "Aguila", "Salamandra"};
-  bool opcionElegida = false;
+  bool opcionElegida = false, turnoJugador1 = false;
 
-  cout << "IRWIN'S REVENGE - FASE DE EXPEDICION \n "
+  cout << "Ingrese el nombre del jugador numero 1: ";
+  cin >> jugador1;
+
+  cout << "Ingrese el nombre del jugador numero 2: ";
+  cin >> jugador2;
+
+  // SORTEO ORDEN JUGADORES
+
+  lanzarDosDados(dadoOrdenJugadores);
+  cout << jugador1 << " lanza un dado y sale: " << dadoOrdenJugadores[0]
+       << endl;
+  cout << jugador2 << " lanza un dado y sale: " << dadoOrdenJugadores[1]
+       << endl;
+
+  while (dadoOrdenJugadores[0] == dadoOrdenJugadores[1]) {
+    cout << "Se repite porque fue empate" << endl;
+
+    lanzarDosDados(dadoOrdenJugadores);
+    cout << jugador1 << " lanza un dado y sale: " << dadoOrdenJugadores[0]
+         << endl;
+    cout << jugador2 << " lanza un dado y sale: " << dadoOrdenJugadores[1]
+         << endl;
+  }
+
+  if (dadoOrdenJugadores[0] < dadoOrdenJugadores[1]) {
+    cout << "El primero en jugar es " << jugador1 << endl;
+    turnoJugador1 = true;
+  } else if (dadoOrdenJugadores[1] < dadoOrdenJugadores[0]) {
+    cout << "El primero en jugar es " << jugador2 << endl;
+    turnoJugador1 = false;
+  }
+
+  //  FASE DE EXPEDICION
+  cout << "\n IRWIN'S REVENGE - FASE DE EXPEDICION \n "
           "----------------------------------------------------------------"
           "----"
        << endl;
-  cout << "STEVE: \n ESTATUILLAS: " << endl;
-  cout << "IRWIN: \n ESTATUILLAS: " << endl;
 
-  for (int i = 0; i < NUM_ESTATUILLAS; i++) {
-    cout << i + 1 << ") " << estatuillas[i] << endl;
-  };
+  while (contadorEstatuillasObtenidas != 5) {
+    cout << jugador1 << ": \n ESTATUILLAS: " << endl;
+    for (int i = 0; i <= 4; i++) {
+      cout << jugador1Estatuillas[i] << "\n";
+    }
+    cout << jugador2 << ": \n ESTATUILLAS: " << endl;
+    for (int i = 0; i <= 4; i++) {
+      cout << jugador2Estatuillas[i] << "\n";
+    }
+    cout << "---------------------- \n\n" << endl;
 
-  cout << "Seleccione la estatuilla que quiere escoger: ";
-  cin >> opcion;
+    if (turnoJugador1) {
+      cout << "---> TURNO DE: " << jugador1 << endl;
+    } else {
+      cout << "---> TURNO DE: " << jugador2 << endl;
+    }
 
-  switch (opcion) {
-    case 1:
-      /* code */
-      cout << "ESTATUILLA OBJETIVO: Hormiga" << endl;
-      cout << "CONDICION DE OBTENCION: Al menos dos dados deben ser menores a "
-              "5."
-           << endl;
-      lanzarDosDados(dadosExpedicion);
-      if (dadosExpedicion[0] < 5 && dadosExpedicion[1] < 5) {
-        estatuillasSteve[0] = "Hormiga";
-        cout << "Ganaste la estatuilla de Hormiga" << endl;
+    for (int i = 0; i < NUM_ESTATUILLAS; i++) {
+      if (estatuillasDisponibles[i]) {
+        cout << i + 1 << ") " << estatuillas[i] << endl;
       }
+    };
 
-      break;
-    case 2:
-      /* code */
-      cout << "ESTATUILLA OBJETIVO: Cangrejo" << endl;
-      cout << "CONDICION DE OBTENCION: Uno de sus dados debe ser par y otro de "
-              "sus dados impar."
-           << endl;
-      lanzarDosDados(dadosExpedicion);
-      if ((dadosExpedicion[0] % 2 != 0 && dadosExpedicion[1] % 2 == 0) ||
-          (dadosExpedicion[0] % 2 == 0 && dadosExpedicion[1] % 2 != 0)) {
-        cout << "Ganaste la estatuilla de Cangrejo" << endl;
-      }
-      break;
-    case 3:
-      /* code */
-      cout << "ESTATUILLA OBJETIVO: Medusa" << endl;
-      cout << "CONDICION DE OBTENCION: La suma de dos de los dados lanzados "
-              "debe dar exactamente siete."
-           << endl;
-      lanzarDosDados(dadosExpedicion);
-      if (dadosExpedicion[0] + dadosExpedicion[1] == 7) {
-        cout << "Ganaste la estatuilla de Medusa" << endl;
-      }
+    cout << "Seleccione la estatuilla que quiere escoger: ";
+    cin >> opcion;
 
-      break;
-    case 4:
-      /* code */
-      cout << "ESTATUILLA OBJETIVO: Aguila" << endl;
-      cout << "CONDICION DE OBTENCION: Dos de los dados lanzados deberan "
-              "devolver los numeros 1 y 10."
-           << endl;
-      lanzarDosDados(dadosExpedicion);
-      if (dadosExpedicion[0] == 10 && dadosExpedicion[1] == 1 ||
-          dadosExpedicion[1] == 10 && dadosExpedicion[0] == 1) {
-        cout << "Ganaste la estatuilla de Aguila" << endl;
-      }
+    switch (opcion) {
+      case 1:
+        cout << "ESTATUILLA OBJETIVO: Hormiga" << endl;
+        cout
+            << "CONDICION DE OBTENCION: Al menos dos dados deben ser menores a "
+               "5."
+            << endl;
+        lanzarDosDados(dadosExpedicion);
+        if (dadosExpedicion[0] < 5 && dadosExpedicion[1] < 5) {
+          cout << "Ganaste la estatuilla de Hormiga" << endl;
+          if (turnoJugador1) {
+            jugador1Estatuillas[0] = "Hormiga";
+          } else {
+            jugador2Estatuillas[0] = "Hormiga";
+          }
+          estatuillasDisponibles[0] = false;
+          contadorEstatuillasObtenidas++;
+        }
+        if (turnoJugador1) {
+          turnoJugador1 = false;
+        } else {
+          turnoJugador1 = true;
+        }
 
-      break;
-    case 5:
-      /* code */
-      cout << "ESTATUILLA OBJETIVO: Salamandra" << endl;
-      cout << "CONDICION DE OBTENCION: Los dados lanzados deben ser "
-              "consecutivos. Por ejemplo: (1 y 2) (2 y 3) (9 y 10)."
-           << endl;
-      lanzarDosDados(dadosExpedicion);
-      if (dadosExpedicion[0] && dadosExpedicion[1] == dadosExpedicion[0] + 1 ||
-          dadosExpedicion[1] && dadosExpedicion[0] == dadosExpedicion[1] + 1) {
-        cout << "Ganaste la estatuilla de Aguila" << endl;
-      }
+        break;
+      case 2:
+        cout << "ESTATUILLA OBJETIVO: Cangrejo" << endl;
+        cout << "CONDICION DE OBTENCION: Uno de sus dados debe ser par y otro "
+                "de "
+                "sus dados impar."
+             << endl;
+        lanzarDosDados(dadosExpedicion);
+        if ((dadosExpedicion[0] % 2 != 0 && dadosExpedicion[1] % 2 == 0) ||
+            (dadosExpedicion[0] % 2 == 0 && dadosExpedicion[1] % 2 != 0)) {
+          cout << "Ganaste la estatuilla de Cangrejo" << endl;
+          if (turnoJugador1) {
+            jugador1Estatuillas[1] = "Cangrejo";
+          } else {
+            jugador2Estatuillas[1] = "Cangrejo";
+          }
+          estatuillasDisponibles[1] = false;
+          contadorEstatuillasObtenidas++;
+        }
+        if (turnoJugador1) {
+          turnoJugador1 = false;
+        } else {
+          turnoJugador1 = true;
+        }
+        break;
+      case 3:
+        cout << "ESTATUILLA OBJETIVO: Medusa" << endl;
+        cout << "CONDICION DE OBTENCION: La suma de dos de los dados lanzados "
+                "debe dar exactamente siete."
+             << endl;
+        lanzarDosDados(dadosExpedicion);
+        if (dadosExpedicion[0] + dadosExpedicion[1] == 7) {
+          cout << "Ganaste la estatuilla de Medusa" << endl;
+          if (turnoJugador1) {
+            jugador1Estatuillas[2] = "Medusa";
+          } else {
+            jugador2Estatuillas[2] = "Medusa";
+          }
+          estatuillasDisponibles[2] = false;
+          contadorEstatuillasObtenidas++;
+        }
+        if (turnoJugador1) {
+          turnoJugador1 = false;
+        } else {
+          turnoJugador1 = true;
+        }
 
-      break;
-    default:
-      cout << "Opcion Invalida.";
-      break;
+        break;
+      case 4:
+        cout << "ESTATUILLA OBJETIVO: Aguila" << endl;
+        cout << "CONDICION DE OBTENCION: Dos de los dados lanzados deberan "
+                "devolver los numeros 1 y 10."
+             << endl;
+        lanzarDosDados(dadosExpedicion);
+        if (dadosExpedicion[0] == 10 && dadosExpedicion[1] == 1 ||
+            dadosExpedicion[1] == 10 && dadosExpedicion[0] == 1) {
+          cout << "Ganaste la estatuilla de Aguila" << endl;
+          if (turnoJugador1) {
+            jugador1Estatuillas[3] = "Aguila";
+          } else {
+            jugador2Estatuillas[3] = "Aguila";
+          }
+          estatuillasDisponibles[3] = false;
+          contadorEstatuillasObtenidas++;
+        }
+        if (turnoJugador1) {
+          turnoJugador1 = false;
+        } else {
+          turnoJugador1 = true;
+        }
+
+        break;
+      case 5:
+        cout << "ESTATUILLA OBJETIVO: Salamandra" << endl;
+        cout << "CONDICION DE OBTENCION: Los dados lanzados deben ser "
+                "consecutivos. Por ejemplo: (1 y 2) (2 y 3) (9 y 10)."
+             << endl;
+        lanzarDosDados(dadosExpedicion);
+        if (dadosExpedicion[0] &&
+            dadosExpedicion[1] == dadosExpedicion[0] + 1) {
+          cout << "Ganaste la estatuilla de Salamandra" << endl;
+          if (turnoJugador1) {
+            jugador1Estatuillas[4] = "Salamandra";
+          } else {
+            jugador2Estatuillas[4] = "Salamandra";
+          }
+          estatuillasDisponibles[4] = false;
+          contadorEstatuillasObtenidas++;
+        }
+        if (turnoJugador1) {
+          turnoJugador1 = false;
+        } else {
+          turnoJugador1 = true;
+        }
+
+        break;
+      default:
+        cout << "Opcion Invalida.";
+        break;
+    }
+
+    cout << "DADO 1: " << dadosExpedicion[0] << endl;
+    cout << "DADO 2: " << dadosExpedicion[1] << endl;
   }
-
-  cout << "DADO 1: " << dadosExpedicion[0] << endl;
-  cout << "DADO 2: " << dadosExpedicion[1] << endl;
 };
 
 void showStatistics() {
